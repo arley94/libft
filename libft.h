cec30925b9652f1776a04e01aab3f6c8827b390a
acoto-gu <acoto-gu@c2r9s4.42madrid.com>
2023-09-21 18:05:01 +0200

    añadir dos funciones bonus

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: acoto-gu <acoto-gu@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 19:40:15 by acoto-gu          #+#    #+#             */
-/*   Updated: 2023/09/21 11:28:45 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2023/09/21 17:53:02 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define LIBFT_H
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -49,4 +55,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 #endif
