01329ce0ddaee8d2fd00904d7b2b1f348610871d
acoto-gu <arleycoto94@gmail.com>
2023-09-25 14:46:07 +0200

    update ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acoto-gu <acoto-gu@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 13:01:49 by acoto-gu          #+#    #+#             */
-/*   Updated: 2023/09/19 13:17:41 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2023/09/25 14:45:24 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strdup(const char *s)
 	size_t	len;
 
 	len = ft_strlen(s);
-	s2 = (char *) malloc((sizeof(char) * len) + 1);
+	s2 = (char *) malloc(sizeof(char) * (len + 1));
 	if (!s2)
 		return (NULL);
 	ft_strlcpy(s2, s, len + 1);
