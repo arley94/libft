a7c44c372045c79b411f5c1af8fead8c765ffc90
acoto-gu <arleycoto94@gmail.com>
2023-09-25 13:04:01 +0200

    update strlcpy and strlcat

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acoto-gu <acoto-gu@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 21:15:02 by acoto-gu          #+#    #+#             */
-/*   Updated: 2023/09/18 08:41:39 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2023/09/25 12:15:09 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	unsigned int	i;
-	unsigned int	srclen;
+	size_t	i;
+	size_t	srclen;
 
 	srclen = ft_strlen(src);
 	i = 0;
-	if (size == 0)
-		return (srclen);
-	while (i < size - 1 && src[i] != '\0')
+	if (size != 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (src[i] && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
 	return (srclen);
 }
