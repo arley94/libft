1c7033013379f4c1acd28fbd4eba4a3ebe478da1
acoto-gu <arleycoto94@gmail.com>
2023-09-25 20:21:56 +0200

    update ft_split

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acoto-gu <acoto-gu@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 09:35:09 by acoto-gu          #+#    #+#             */
-/*   Updated: 2023/09/20 17:38:56 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2023/09/25 18:03:53 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,30 +28,56 @@ static int	count_words(char const *s, char c)
 	return (word_count);
 }
 
+static char	**free_arr(char **str_arr, int n_words)
+{
+	int	i;
+
+	i = 0;
+	while (i < n_words)
+		free (str_arr[i++]);
+	free(str_arr);
+	return (0);
+}
+
+static char	**split(char **str_arr, char const *s, char c, int n_words)
+{
+	int	i;
+	int	num_words;
+	int	w_len;
+
+	i = 0;
+	num_words = 0;
+	w_len = 0;
+	while (num_words < n_words)
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
+		{
+			i++;
+			w_len++;
+		}
+		str_arr[num_words] = ft_substr(s, i - w_len, w_len);
+		if (!str_arr[num_words])
+			return (free_arr(str_arr, num_words));
+		w_len = 0;
+		num_words++;
+	}
+	str_arr[num_words] = NULL;
+	return (str_arr);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**ptr;
-	int		s_index;
-	int		i;
-	int		i_ptr;
+	int		n_words;
 
-	ptr = malloc(sizeof(char *) * (count_words(s, c) + 1));
-	if (!ptr || !s)
+	if (!s)
 		return (NULL);
-	i = 0;
-	while (s[i] == c && s[i])
-		i++;
-	i_ptr = 0;
-	while (s[i])
-	{
-		if (s[i] != c && (i == 0 || s[i - 1] == c))
-			s_index = i;
-		else if (s[i] == c && s[i - 1] != c)
-			ptr[i_ptr++] = ft_substr(s, s_index, i - s_index);
-		if (s[i] != c && s[i + 1] == '\0')
-			ptr[i_ptr++] = ft_substr(s, s_index, i - s_index + 1);
-		i++;
-	}
-	ptr[i_ptr] = 0;
+	n_words = count_words(s, c);
+	ptr = malloc(sizeof(char *) * (n_words + 1));
+	if (!ptr)
+		return (NULL);
+	ptr = split(ptr, s, c, n_words);
 	return (ptr);
 }
