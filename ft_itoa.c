df0d751672104cd69355d4f0578f54480bdc9ceb
acoto-gu <arleycoto94@gmail.com>
2023-09-25 20:57:49 +0200

    update ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acoto-gu <acoto-gu@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 18:30:49 by acoto-gu          #+#    #+#             */
-/*   Updated: 2023/09/21 09:31:08 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2023/09/25 20:52:46 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_char(long int number)
+static size_t	count_char(unsigned long number)
 {
-	int			count;
+	size_t	count;
 
 	count = 1;
 	while (number >= 10)
@@ -27,19 +27,19 @@ static int	count_char(long int number)
 
 char	*ft_itoa(int n)
 {
-	int			n_chars;
-	char		*ptr;
-	long int	number;
-	int			sign;
+	size_t			n_chars;
+	char			*ptr;
+	long int		number;
+	unsigned int	sign;
 
 	sign = 0;
-	number = (long int)n;
+	number = (long)n;
 	if (number < 0)
 	{
 		sign = 1;
 		number = number * -1;
 	}
-	n_chars = count_char(number);
+	n_chars = count_char((unsigned long)number);
 	ptr = malloc(sizeof(char) * (n_chars + sign + 1));
 	if (!ptr)
 		return (NULL);
@@ -48,7 +48,7 @@ char	*ft_itoa(int n)
 	ptr[n_chars + sign] = '\0';
 	while (n_chars > 0)
 	{
-		ptr[n_chars-- + sign - 1] = "0123456789"[(number % 10 - 0)];
+		ptr[n_chars-- + sign - 1] = number % 10 + '0';
 		number = number / 10;
 	}
 	return (ptr);
